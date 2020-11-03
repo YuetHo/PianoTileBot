fdae04d80ad17022900e8448bb08f6201094d412
Clasless <58229896+Clasless@users.noreply.github.com>
2020-11-03 01:16:32 -0500

    Coding.

--- PianoTileBot.py
+++ PianoTileBot.py
@@ -1,16 +1,33 @@
-import sys
-from math import cos, radians
-# Piano Tile Autobot
-# Create a string with spaces proportional to a cosine of x in degrees
-def make_dot_string(x):
-    rad = radians(x)
-    numspaces = int(20*cos(radians(x)) + 20)
-    st = ' ' * numspaces + 'o'
-    return st
+from pyautogui import *
+import pyautogui
+import keyboard
+import time
+import random
+import win32api, win32con
 
-def main():
-    for i in range(0,1800,12):
-        s = make_dot_string(i)
-        print(s)
+# Tile 1 = X: 800 Y: 650 RGB: ( 36, 37, 37)
+# Tile 2 = X: 900 Y: 650 RGB: ( 36, 37, 37)
+# Tile 3 = X: 1000 Y: 650 RGB: ( 36, 37, 37)
+# Tile 4 = X: 1100 Y: 650 RGB: ( 36, 37, 37)
 
-main()
+x1 = 800
+x2 = 900
+x3 = 1000
+x4 = 1100
+y1 = 650
+
+def click(x,y):
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
+    time.sleep(0.01)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+
+while keyboard.is_pressed('q') == False:
+    if pyautogui.pixel(x1, y1)[0] == 17:
+        click(x1,y1)
+    if pyautogui.pixel(x2, y1)[0] == 17:
+        click(x2,y1)
+    if pyautogui.pixel(x3, y1)[0] == 17:
+        click(x3,y1)
+    if pyautogui.pixel(x4, y1)[0] == 17:
+        click(x4,y1)
